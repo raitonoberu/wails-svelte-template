d766172d41b2438a1d89e685e1d0e7e7f86bd70f
Denis <raitonoberu@mail.ru>
2022-01-02 00:52:39 +0700

    Remove serving & update README

--- README.md
+++ README.md
@@ -16,5 +16,4 @@ To generate a platform native package, add the `-package` flag.
 ## Live Development
 
 To run in live development mode, run `wails dev` in the project directory. In another terminal, go into the `frontend` 
-directory and run `npm run dev`. The frontend dev server will run on http://localhost:34115. Connect to this
-in your browser and connect to your application. 
+directory and run `npm run dev`. Now every change you make to the frontend should hot-reload the UI.
--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -7,26 +7,6 @@ import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	let server;
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
 
 export default {
 	input: 'src/main.js',
@@ -57,10 +37,6 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
